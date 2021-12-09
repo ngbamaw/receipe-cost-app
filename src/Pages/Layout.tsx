2b13ee6c62f7a88cd4fc5b3200d749327fc85107
PellYm785 <williamliya@hotmail.fr>
2021-12-09 19:11:20 +0100

    create layout

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link, useMatch, useResolvedPath, Outlet } from 'react-router-dom';
+import ReceipeIcon from '../assets/img/receipe-icon.png';
+import IngredientIcon from '../assets/img/ingredient-icon.png';
+import StyledLayout from './styles/Layout';
+import type { LinkProps } from 'react-router-dom';
+import classNames from 'classnames';
+
+interface CustomLinkProps extends LinkProps {
+    icon: string;
+    children: string;
+}
+function CustomLink({ children, to, icon, ...props }: CustomLinkProps) {
+    const resolved = useResolvedPath(to);
+    const match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <Link to={to} className={classNames('btn-menu', { selected: match })} {...props}>
+            <img className="icon-menu" src={icon} alt={children} />
+            <p>{children}</p>
+        </Link>
+    );
+}
 
 const Layout: React.FC = () => {
-    return <div></div>;
+    return (
+        <StyledLayout>
+            <Outlet />
+            <div className="menu-bar">
+                <CustomLink to="/receipes" icon={ReceipeIcon}>
+                    Recettes
+                </CustomLink>
+                <CustomLink to="/ingredients" icon={IngredientIcon}>
+                    Ingredients
+                </CustomLink>
+            </div>
+        </StyledLayout>
+    );
 };
 
 export default Layout;
---
+++ src/Pages/styles/Layout.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+    .menu-bar {
+        position: fixed;
+        bottom: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        width: 100%;
+        box-shadow: 0px -3px 12px rgba(0, 0, 0, 0.25);
+        padding: 10px 0;
+        .btn-menu {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            font-family: Roboto;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 18px;
+            text-decoration: none;
+            color: black;
+            text-align: center;
+            .icon-menu {
+                max-width: 100px;
+            }
+        }
+
+        .btn-menu.selected {
+            color: #ff7a00;
+        }
+    }
+`;
+
+export default Layout;
