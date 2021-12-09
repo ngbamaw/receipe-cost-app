5d76c7a8af19ddb7d8bee712c57340e04ff7ff31
PellYm785 <williamliya@hotmail.fr>
2021-12-09 23:49:01 +0100

    add list and header

--- src/Pages/Receipes.tsx
+++ src/Pages/Receipes.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import List from '@mui/material/List';
+import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Add';
+import { Avatar, IconButton, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
+import StyledReceipes from './styles/Receipes';
+import AvatarImage from '../assets/img/img-avatar.png';
+
+const data = [
+    {
+        image: AvatarImage,
+        title: 'Tiramisu',
+    },
+    {
+        image: AvatarImage,
+        title: 'Tiramisu',
+    },
+    {
+        image: AvatarImage,
+        title: 'Tiramisu',
+    },
+];
 
 const Receipes: React.FC = () => {
-    return <div></div>;
+    return (
+        <StyledReceipes>
+            <header className="header-receipes">
+                <div className="search-input">
+                    <SearchIcon className="search-icon" />
+                    <input type="text" placeholder="Search" />
+                </div>
+                <IconButton className="add-btn">
+                    <AddIcon />
+                </IconButton>
+            </header>
+            <List className="receipes-list">
+                {data.map(({ image, title }) => (
+                    <ListItemButton>
+                        <ListItemAvatar className="receipe-image">
+                            <Avatar
+                                sx={{ height: '70px', width: '70px' }}
+                                alt={`Avatar n°1`}
+                                src={image}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText className="receipe-title" primary={title} />
+                    </ListItemButton>
+                ))}
+            </List>
+        </StyledReceipes>
+    );
 };
 
 export default Receipes;
---
+++ src/Pages/styles/Receipes.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+const Receipes = styled.div`
+    display: flex;
+    flex-direction: column;
+    .header-receipes {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 24px 0;
+        .search-input {
+            background-color: white;
+            padding: 4px;
+            box-shadow: 0px 0px 11px rgba(0, 0, 0, 0.25);
+            border-radius: 15px;
+            display: flex;
+            height: fit-content;
+            width: 70%;
+            margin-right: 10px;
+            .search-icon {
+                color: #b0b0b0;
+            }
+            input {
+                border: none;
+                outline: none;
+                width: 100%;
+                &::placeholder {
+                    color: #b0b0b0;
+                    font-weight: bold;
+                }
+            }
+        }
+    }
+
+    .receipes-list {
+        max-height: 50%;
+        overflow-y: auto;
+        .receipe-image {
+            margin-right: 38px;
+        }
+        .receipe-title {
+            span {
+                font-weight: bold;
+            }
+        }
+    }
+`;
+
+export default Receipes;
