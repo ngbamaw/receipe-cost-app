0cd858faa7f3c77554990492ad4d625f1e32109b
PellYm785 <williamliya@hotmail.fr>
2021-12-13 11:27:04 +0100

    create receipe page

--- src/Pages/Receipe.tsx
+++ src/Pages/Receipe.tsx
@@ -1,7 +1,82 @@
 import React from 'react';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import { IconButton } from '@mui/material';
+import StyledReceipe from './styles/Receipe';
+import AvatarImage from '../assets/img/img-avatar.png';
 
+const data = {
+    title: 'Tortilla de patatas',
+    img: AvatarImage,
+    ingredients: [
+        {
+            name: 'Patatas',
+            amount: '1',
+            unit: 'kg',
+            img: AvatarImage,
+            price: '1.50',
+        },
+        {
+            name: 'Patatas',
+            amount: '1',
+            unit: 'kg',
+            img: AvatarImage,
+            price: '1.50',
+        },
+    ],
+};
 const Receipe: React.FC = () => {
-    return <div></div>;
+    const { title, img, ingredients } = data;
+    return (
+        <StyledReceipe>
+            <IconButton className="back-btn">
+                <ArrowBackIcon fontSize="large" />
+            </IconButton>
+            <img className="receipe-image" src={img} alt={title} />
+            <header className="receipe-header">
+                <h1 className="receipe-title">{title}</h1>
+                <IconButton className="edit-btn">
+                    <EditIcon fontSize="large" />
+                </IconButton>
+            </header>
+            <div className="receipe-ingredients">
+                <ul className="ingredient-list">
+                    {ingredients.map((ingredient, index) => (
+                        <li className="ingredient-item" key={index}>
+                            <div className="ingredient-label">
+                                <img
+                                    className="ingredient-image"
+                                    src={ingredient.img}
+                                    alt={ingredient.name}
+                                />
+                                <p className="ingredient-name">{ingredient.name}</p>
+                            </div>
+                            <div className="ingredient-amount">
+                                <span className="ingredient-number">{ingredient.amount}</span>
+                                <span className="ingredient-unit">{ingredient.unit}</span>
+                            </div>
+                            <div className="ingredient-price">
+                                <span>{ingredient.price}</span>
+                                <span>€</span>
+                            </div>
+                        </li>
+                    ))}
+                    <li className="add-line">
+                        <IconButton className="add-btn">
+                            <AddIcon fontSize="large" />
+                        </IconButton>
+                        <p>Ajouter</p>
+                    </li>
+                </ul>
+
+                <div className="price">
+                    <p className="price-number">25€</p>
+                    <p className="price-label">Prix total</p>
+                </div>
+            </div>
+        </StyledReceipe>
+    );
 };
 
 export default Receipe;
---
+++ src/Pages/styles/Receipe.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+const Receipe = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+    overflow: hidden;
+    .back-btn {
+        align-self: flex-start;
+        margin-top: 24px;
+        margin-left: 24px;
+        color: black;
+    }
+    .receipe-image {
+        width: 125px;
+        height: 125px;
+        border-radius: 50%;
+        margin-bottom: 32px;
+    }
+    .receipe-header {
+        display: flex;
+        align-items: center;
+        position: relative;
+        margin-bottom: 24px;
+        .edit-btn {
+            color: black;
+            position: absolute;
+            right: 0;
+            transform: translateX(100%);
+        }
+    }
+    .receipe-ingredients {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        overflow: auto;
+        .ingredient-list {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            .ingredient-item {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: space-between;
+                padding: 20px 16px;
+                .ingredient-label {
+                    display: flex;
+                    align-items: center;
+                    gap: 24px;
+                    font-weight: normal;
+                    font-size: 18px;
+                    line-height: 21px;
+                    .ingredient-image {
+                        width: 55px;
+                        height: 50px;
+                        border-radius: 22px;
+                    }
+                }
+                font-weight: bold;
+                font-size: 18px;
+                .ingredient-amount {
+                    .ingredient-unit {
+                        color: #7e7e7e;
+                    }
+                }
+            }
+            .add-line {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                padding: 20px 16px;
+                gap: 24px;
+                font-size: 18px;
+                .add-btn {
+                    background: #cecece;
+                    border-radius: 22px;
+                    width: 55px;
+                    height: 50px;
+                    color: white;
+                }
+            }
+        }
+        .price {
+            margin-top: auto;
+            margin-bottom: 24px;
+            .price-number {
+                font-weight: bold;
+                font-size: 48px;
+            }
+            .price-label {
+                font-weight: bold;
+                font-size: 14px;
+                text-align: center;
+                color: #b0b0b0;
+            }
+        }
+    }
+`;
+
+export default Receipe;
