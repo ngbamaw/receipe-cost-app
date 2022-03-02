2003448ef966ca700327148f0b450bf4cb6ccda1
PellYm785 <williamliya@hotmail.fr>
2022-03-02 01:24:17 +0100

    add state to addingredient component

--- src/Pages/AddIngredient.tsx
+++ src/Pages/AddIngredient.tsx
@@ -8,6 +8,8 @@ import SwipeableViews from 'react-swipeable-views';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
 import { AddIngredientSection } from './styles/Receipe';
+import { Ingredient, ReceipeEntry, useIngredientsQuery } from '../generated/graphql';
+import { getUrlForImage } from '../utils';
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
@@ -18,60 +20,82 @@ const Transition = React.forwardRef(function Transition(
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export interface Ingredient {
-    name: string;
-    amount: string;
-    unit: string;
-    img: string;
-    price: string;
+interface Result {
+    ingredient: Ingredient;
+    quantity: number;
 }
 interface IAddIngredientProps {
     open: boolean;
-    handleClose: () => void;
-    handleNext: () => void;
-    handleBack: () => void;
-    activeStep: number;
-    ingredients: Ingredient[];
+    onClose?: () => void;
+    onNext?: () => void;
+    onBack?: () => void;
+    onFinished: (value: Result) => void;
+    receipeEntries: ReceipeEntry[];
 }
 const AddIngredient: React.FC<IAddIngredientProps> = ({
     open,
-    handleClose,
-    handleNext,
-    handleBack,
-    activeStep,
-    ingredients,
+    onClose,
+    onNext,
+    onBack,
+    receipeEntries = [],
+    onFinished,
 }) => {
+    const [openInternal, setOpenInternal] = React.useState(false);
+    const [activeStep, setActiveStepInternal] = React.useState(0);
+    const [selectedIngredient, setSelectedIngredient] = React.useState<number>(-1);
+    const [quantity, setQuantity] = React.useState<number>(0);
+    const numberOfStep = 2;
+
+    const handleNext = () => {
+        if (onNext) onNext();
+        else setActiveStepInternal((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        if (onBack) onBack();
+        else setActiveStepInternal((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const { data } = useIngredientsQuery();
+    const ingredients = data?.ingredients?.filter((item) =>
+        receipeEntries.every((r) => r.ingredient?.id !== item?.id),
+    );
+
+    React.useEffect(() => {
+        if (activeStep === -1 || activeStep === numberOfStep) {
+            setOpenInternal(false);
+            setActiveStepInternal(0);
+            if (activeStep === numberOfStep)
+                onFinished({
+                    ingredient: ingredients?.[selectedIngredient] as Ingredient,
+                    quantity,
+                });
+        }
+    }, [activeStep, selectedIngredient, quantity, ingredients, onFinished]);
+
+    React.useEffect(() => {
+        setOpenInternal(open);
+    }, [open]);
+
     return (
-        <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <Dialog fullScreen open={openInternal} onClose={onClose} TransitionComponent={Transition}>
             <AddIngredientSection>
                 <SwipeableViews className="swiper" index={activeStep}>
                     <div className="step-1">
                         <ul className="ingredient-list">
-                            {ingredients.map((ingredient, index) => (
-                                <li className="ingredient-item" key={index}>
+                            {ingredients?.map((ingredient, index) => (
+                                <li
+                                    className="ingredient-item"
+                                    onClick={() => setSelectedIngredient(index)}
+                                    key={index}
+                                >
                                     <div className="ingredient-label">
                                         <img
                                             className="ingredient-image"
-                                            src={ingredient.img}
-                                            alt={ingredient.name}
+                                            src={getUrlForImage(ingredient?.image?.url)}
+                                            alt={ingredient?.name}
                                         />
-                                        <p className="ingredient-name">{ingredient.name}</p>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="step-2">
-                        <ul className="ingredient-list">
-                            {ingredients.map((ingredient, index) => (
-                                <li className="ingredient-item" key={index}>
-                                    <div className="ingredient-label">
-                                        <img
-                                            className="ingredient-image"
-                                            src={ingredient.img}
-                                            alt={ingredient.name}
-                                        />
-                                        <p className="ingredient-name">{ingredient.name}</p>
+                                        <p className="ingredient-name">{ingredient?.name}</p>
                                     </div>
                                 </li>
                             ))}
@@ -99,7 +123,7 @@ const AddIngredient: React.FC<IAddIngredientProps> = ({
                         <p>Ajouter un ingrédient</p>
                         <MobileStepper
                             variant="dots"
-                            steps={3}
+                            steps={numberOfStep}
                             position="static"
                             activeStep={activeStep}
                             sx={{ maxWidth: 400, flexGrow: 1 }}
