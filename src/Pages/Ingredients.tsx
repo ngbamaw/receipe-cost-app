3e8a4b6d41fd94fb0585b51143f43f9ea9cc872e
PellYm785 <williamliya@hotmail.fr>
2022-03-06 19:15:55 +0100

    list data from server and create ingredient

--- src/Pages/Ingredients.tsx
+++ src/Pages/Ingredients.tsx
@@ -4,38 +4,64 @@ import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import StarIcon from '@mui/icons-material/Star';
-import { Avatar, IconButton, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+    Avatar,
+    FormControl,
+    IconButton,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemText,
+    TextField,
+    Select,
+    MenuItem,
+    InputLabel,
+} from '@mui/material';
+import { useNavigate, useLocation, useParams } from 'react-router-dom';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import StyledIngredients from './styles/Ingredients';
-import AvatarImage from '../assets/img/img-avatar.png';
 import setLongPress from '../useLongPress';
+import {
+    useIngredientsQuery,
+    useCreateIngredientMutation,
+    EnumIngredientUnit,
+} from '../generated/graphql';
+import { getUrlForImage, useUploadMutation } from '../utils';
+import PicturePicker, { Picture } from './PicturePicker';
 
-const data = [
-    {
-        image: AvatarImage,
-        title: 'Oeuf',
-    },
-    {
-        image: AvatarImage,
-        title: 'Oeuf',
-    },
-    {
-        image: AvatarImage,
-        title: 'Oeuf',
-    },
-];
+const getIndex = (element: HTMLElement) => {
+    let current = element;
+    while (!current.dataset.index) {
+        current = current.parentElement as HTMLElement;
+    }
+
+    return parseInt(current.dataset.index);
+};
 
 const Ingredients: React.FC = () => {
-    const [selectedIndex, setSelectedIndex] = React.useState<number[]>([]);
+    const [selectedIndexies, setSelectedIndexies] = React.useState<number[]>([]);
     const [isLongPressing, setIsLongPressing] = React.useState(false);
+    const [newIngredient, setNewIngredient] = React.useState('');
+    const [unit, setUnit] = React.useState<EnumIngredientUnit>(EnumIngredientUnit.Gram);
+    const [openPhotoPicker, setOpenPhotoPicker] = React.useState(false);
+    const [newPicture, setNewPicture] = React.useState<Picture>();
     const navigate = useNavigate();
+    const location = useLocation();
+    const { data } = useIngredientsQuery();
+    const { ingredientId } = useParams();
+    const createIngredient = useCreateIngredientMutation();
+    const upload = useUploadMutation();
 
+    /*
     const onLongPress = (index: number) => () => {
         if (!isLongPressing) {
             setSelectedIndex([index]);
             setIsLongPressing(true);
         }
     };
+
+
     const onClick = (index: number) => () => {
         if (isLongPressing) {
             if (!selectedIndex.includes(index)) {
@@ -44,16 +70,65 @@ const Ingredients: React.FC = () => {
                 setSelectedIndex(selectedIndex.filter((item) => item !== index));
             }
         } else {
-            navigate(`/ingredients/${index}`);
+            navigate(`${location.pathname}/${index}`);
         }
     };
+    */
+
+    const onPicture = async (picture: Picture) => {
+        setNewPicture(picture);
+    };
+
+    const onLongPress = React.useCallback(
+        (e) => {
+            const index = getIndex(e.target as HTMLElement);
+            if (!isLongPressing) {
+                setSelectedIndexies([index]);
+                setIsLongPressing(true);
+            }
+        },
+        [isLongPressing],
+    );
+
+    const onClick = (e: any) => {
+        const index = getIndex(e.target as HTMLElement);
+        if (isLongPressing) {
+            if (!selectedIndexies.includes(index)) {
+                setSelectedIndexies([...selectedIndexies, index]);
+            } else {
+                setSelectedIndexies(selectedIndexies.filter((item) => item !== index));
+            }
+        } else {
+            navigate(`${location.pathname}/${data?.ingredients?.[index]?.id}`);
+        }
+    };
+    const onLongPressListener = setLongPress(onLongPress, onClick);
     React.useEffect(() => {
-        if (isLongPressing && selectedIndex.length === 0) {
+        if (isLongPressing && selectedIndexies.length === 0) {
             setIsLongPressing(false);
         }
-    }, [selectedIndex, isLongPressing]);
+    }, [selectedIndexies, isLongPressing]);
+
+    const onCreateIngredient = React.useCallback(
+        async (e) => {
+            e.preventDefault();
+            if (newPicture) {
+                const image = await upload.mutateAsync({ file: newPicture.file });
+                createIngredient.mutate({
+                    input: { data: { name: newIngredient, unit, image: image[0].id } },
+                });
+                setNewIngredient('');
+            }
+        },
+        [newIngredient, createIngredient, unit, newPicture, upload],
+    );
     return (
         <StyledIngredients>
+            <PicturePicker
+                onPicture={onPicture}
+                open={openPhotoPicker}
+                onClose={() => setOpenPhotoPicker(false)}
+            />
             <header className="header-ingredients">
                 <div className="search-input">
                     <SearchIcon className="search-icon" />
@@ -64,29 +139,71 @@ const Ingredients: React.FC = () => {
                         <DeleteIcon fontSize="large" className="del-icon" />
                     </IconButton>
                 ) : (
-                    <IconButton onClick={() => navigate('/ingredients/new')} className="add-btn">
+                    <IconButton
+                        onClick={() => navigate(`${location.pathname}/new`)}
+                        className="add-btn"
+                    >
                         <AddIcon fontSize="large" className="add-icon" />
                     </IconButton>
                 )}
             </header>
-            <h2 className="ingredient-name">Oeuf</h2>
+            {ingredientId && <h2 className="ingredient-name">Oeuf</h2>}
             <List className="ingredients-list">
-                {data.map(({ image, title }, index) => (
+                <ListItemButton className="ingredient-item" key="new">
+                    <ListItemAvatar
+                        onClick={() => setOpenPhotoPicker(true)}
+                        className="ingredient-image picture-picker"
+                    >
+                        {newPicture ? (
+                            <Avatar sx={{ height: '70px', width: '70px' }} src={newPicture.src} />
+                        ) : (
+                            <Avatar sx={{ height: '70px', width: '70px' }}>
+                                <AddAPhotoIcon fontSize="large" />
+                            </Avatar>
+                        )}
+                    </ListItemAvatar>
+                    <TextField
+                        variant="standard"
+                        label="Nouvelle ingredient"
+                        value={newIngredient}
+                        onChange={(e) => setNewIngredient(e.target.value)}
+                    />
+                    <FormControl className="amount-type-container">
+                        <InputLabel id="amount-type-label">Type</InputLabel>
+                        <Select
+                            value={unit}
+                            label="Type"
+                            onChange={(event) => setUnit(event.target.value as EnumIngredientUnit)}
+                        >
+                            <MenuItem value={EnumIngredientUnit.Liter}>Liquide</MenuItem>
+                            <MenuItem value={EnumIngredientUnit.Gram}>Solide</MenuItem>
+                            <MenuItem value={EnumIngredientUnit.Unit}>Unité</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <IconButton className="validate-btn" onClick={onCreateIngredient}>
+                        <CheckIcon fontSize="large" />
+                    </IconButton>
+                    <IconButton className="cancel-btn">
+                        <CloseIcon fontSize="large" />
+                    </IconButton>
+                </ListItemButton>
+                {data?.ingredients?.map((ingredient, index) => (
                     <ListItemButton
-                        {...setLongPress(onLongPress(index), onClick(index))}
-                        selected={isLongPressing && selectedIndex.includes(index)}
-                        className="ingredient-item"
+                        {...onLongPressListener}
+                        selected={isLongPressing && selectedIndexies.includes(index)}
+                        className="receipe-item"
+                        data-index={index}
                     >
                         <ListItemAvatar className="ingredient-image">
                             <Avatar
                                 sx={{ height: '70px', width: '70px' }}
                                 alt={`Avatar n°1`}
-                                src={image}
+                                src={getUrlForImage(ingredient?.image?.url)}
                             />
                         </ListItemAvatar>
-                        <StarIcon className="favorite" />
-                        <ListItemText className="ingredient-title" primary={title} />
-                        <p className="price">5€</p>
+                        {ingredientId && <StarIcon className="favorite" />}
+                        <ListItemText className="ingredient-title" primary={ingredient?.name} />
+                        <p className="ingredient-price">5€</p>
                     </ListItemButton>
                 ))}
             </List>
