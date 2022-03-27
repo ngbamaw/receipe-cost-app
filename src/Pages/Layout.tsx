8ad2ed737612937d48ca0d08360a1c6f4bc475d8
PellYm785 <williamliya@hotmail.fr>
2022-03-28 01:18:29 +0200

    add "magasins" and "marques" sections

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Link, useMatch, useResolvedPath, Outlet } from 'react-router-dom';
+import { Link, useResolvedPath, useLocation, Outlet } from 'react-router-dom';
 import ReceipeIcon from '../assets/img/receipe-icon.png';
 import IngredientIcon from '../assets/img/ingredient-icon.png';
+import StoreIcon from '../assets/img/store-icon.png';
+import BrandIcon from '../assets/img/brand-icon.png';
 import StyledLayout from './styles/Layout';
 import type { LinkProps } from 'react-router-dom';
 import classNames from 'classnames';
@@ -10,9 +12,11 @@ interface CustomLinkProps extends LinkProps {
     icon: string;
     children: string;
 }
+
 function CustomLink({ children, to, icon, ...props }: CustomLinkProps) {
     const resolved = useResolvedPath(to);
-    const match = useMatch({ path: resolved.pathname, end: true });
+    const location = useLocation();
+    const match = location.pathname.includes(resolved.pathname);
 
     return (
         <Link to={to} className={classNames('btn-menu', { selected: match })} {...props}>
@@ -32,6 +36,12 @@ const Layout: React.FC = () => {
                 </CustomLink>
                 <CustomLink to="/ingredients" icon={IngredientIcon}>
                     Ingredients
+                </CustomLink>
+                <CustomLink to="/magasins" icon={StoreIcon}>
+                    Magasins
+                </CustomLink>
+                <CustomLink to="/marques" icon={BrandIcon}>
+                    Marques
                 </CustomLink>
             </div>
         </StyledLayout>
